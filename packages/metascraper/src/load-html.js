fb717b9f6624237d366dcb7d8d1abd1d63342884
Eric Wiener <ericwiener3@gmail.com>
2020-05-30 09:22:39 -0400

    FEAT: changed cheerio dep to 'react-native-cheerio'
    Not a pull request worthy change, but a test to see if this modification
    will allow the package to work or if more changes are needed.

--- packages/metascraper/src/load-html.js
+++ packages/metascraper/src/load-html.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const cheerio = require('cheerio-advanced-selectors').wrap(require('cheerio'))
+const cheerio = require('cheerio-advanced-selectors').wrap(require('react-native-cheerio'));
 
 module.exports = (html = '') =>
   cheerio.load(html, {
     lowerCaseTags: true,
     decodeEntities: true,
     lowerCaseAttributeNames: true
-  })
+  });
